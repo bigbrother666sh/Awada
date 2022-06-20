df2f76baed3f41d7a1f1036309cf3542a3d07e16
bigbrother666 <zeming.zhao@gmail.com>
2022-06-20 16:21:59 +0800

    add first sentense to the memory

--- run.py
+++ run.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     bot = Wechaty(options)
     bot.use([
         DramaPlugin(),
-        DramaZeusPlugin(24.7dc77c87ce71ed73b73d714728672313.86400000.1655779572205.e874c242e1a293ad1972230edbf69e42-36698)
+        DramaZeusPlugin(access_token = "24.7dc77c87ce71ed73b73d714728672313.86400000.1655779572205.e874c242e1a293ad1972230edbf69e42-36698")
     ])
     asyncio.run(bot.start())
